ccd78453480397ae019cb437589537b0f25d2781
Aman11b <amansingh1100b@gmail.com>
2026-06-18 00:11:07 +0530

    creating a layout:

--- app/page.js
+++ app/page.js
@@ -4,12 +4,11 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>HELLOW WORLD</h1>
 
       {/* this will create full page reload */}
       {/* <a href="/cabins">Explore cabins</a> */}
-      <Link href="/cabins">Explore Luxury</Link>
+      {/* <Link href="/cabins">Explore Luxury</Link> */}
     </div>
   );
 }
--- learning.md
+++ learning.md
@@ -1,3 +1,4 @@
 # Next js
 
 - Each page is a react component whihc we export from page.js file
+- whatever is rendered is returned by RootLaoyout
